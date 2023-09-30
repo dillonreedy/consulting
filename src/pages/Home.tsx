19c26a67f2d88272e803a5b0938899b1ab5ccf2e
dillonreedy <dillon.developing@outlook.com>
2023-09-30 15:03:01 -0500

    1 file modified, 1 added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import { Box } from '@mui/material';
+import ImageHalfWidth from '../components/ImageHalfWidth';
 import NavBar from '../components/NavBar';
 
 const Home: React.FC = () => {
   return (
-    <Box sx={{ backgroundColor: 'black', height: '100vh' }}>
+    <Box sx={{ backgroundColor: 'black', height: '100%' }}>
       <NavBar />
-      <h1>Home Page</h1>
+      <ImageHalfWidth imageUrl="https://raw.githubusercontent.com/dillonreedy/consulting/main/src/assets/profile.jpg" />
     </Box>
   );
 };
