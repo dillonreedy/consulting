8817b0312db7ab9196d3ebc94dd0c79fd078213c
dillonreedy <dillon.developing@outlook.com>
2023-10-01 12:55:49 -0500

    1 file modified, 2 deleted

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,30 @@
-import { Box } from '@mui/material';
-import ImageHalfWidth from '../components/ImageHalfWidth';
+import { Box, Grid, Paper } from '@mui/material';
 import NavBar from '../components/NavBar';
 
 const Home: React.FC = () => {
   return (
     <Box sx={{ backgroundColor: 'black', height: '100%' }}>
       <NavBar />
-      <ImageHalfWidth imageUrl="https://raw.githubusercontent.com/dillonreedy/consulting/main/src/assets/profile.jpg" />
+
+      <Grid container justifyContent="center">
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          style={{ display: 'flex', alignItems: 'center' }}
+        >
+          <Paper elevation={3}>
+            <img
+              src="https://raw.githubusercontent.com/dillonreedy/consulting/main/src/assets/profile.jpg"
+              alt="Image Preview"
+              style={{ width: '100%' }}
+            />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          {/* Your content for the right side */}
+        </Grid>
+      </Grid>
     </Box>
   );
 };
